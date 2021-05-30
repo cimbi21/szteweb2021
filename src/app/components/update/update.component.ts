a311defab9c1b182d102f02816a3a5e5eef1087b
cimbi21 <kollath.istvan.tibor@stud.u-szeged.hu>
2021-05-30 10:31:34 +0200

    add

--- src/app/components/update/update.component.ts
+++ src/app/components/update/update.component.ts
@@ -29,17 +29,17 @@ export class UpdateComponent implements OnInit, OnDestroy{
 
   ]
 
-  kuldes: boolean = false;
+  siker: boolean = false;
   id: string | null = null;
   totalAmount: string | null= null;
   paymentMethod: string | null= null;
   description: string | null= null;
-  updating: boolean = false;
   status: string | null= null;
   p: Payment=new Payment(this.id, this.status, this.description, this.paymentMethod, this.totalAmount);
   constructor(private service: PaymentService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.siker=false;
     this.id=this.route.snapshot.paramMap.get('id');
     this.status=this.route.snapshot.paramMap.get('status');
     this.description=this.route.snapshot.paramMap.get('description');
@@ -52,7 +52,7 @@ export class UpdateComponent implements OnInit, OnDestroy{
   }
   update(): void{
     if (this.p.id != null) {
-      this.service.update(this.p.id, this.p).then(r => {console.log(r);});
+      this.service.update(this.p.id, this.p).then(r => {this.siker=true;});
     }
   }
 }
